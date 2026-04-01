e09b2d4329936e210f07262e497478658f4cf1e7
Sofiane <152896793+Naicoram@users.noreply.github.com>
2026-04-01 17:19:50 +0200

    Add files via upload

---
+++ tifosi_complet.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 01 avr. 2026 à 15:08
+-- Version du serveur : 8.4.7
+-- Version de PHP : 8.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tifosi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `achete`
+--
+
+DROP TABLE IF EXISTS `achete`;
+CREATE TABLE IF NOT EXISTS `achete` (
+  `id_client` int DEFAULT NULL,
+  `id_focaccia` int DEFAULT NULL,
+  `jour` date NOT NULL,
+  KEY `focaccia` (`id_focaccia`),
+  KEY `Client` (`id_client`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `boisson`
+--
+
+DROP TABLE IF EXISTS `boisson`;
+CREATE TABLE IF NOT EXISTS `boisson` (
+  `id_boisson` int NOT NULL AUTO_INCREMENT,
+  `nom_boisson` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_marque` int DEFAULT NULL,
+  PRIMARY KEY (`id_boisson`),
+  KEY `Marque` (`id_marque`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int NOT NULL AUTO_INCREMENT,
+  `nom_client` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `age` int NOT NULL,
+  `cp_client` int NOT NULL,
+  PRIMARY KEY (`id_client`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comprend`
+--
+
+DROP TABLE IF EXISTS `comprend`;
+CREATE TABLE IF NOT EXISTS `comprend` (
+  `id_focaccia` int DEFAULT NULL,
+  `id_ingredient` int DEFAULT NULL,
+  KEY `foccacia` (`id_focaccia`),
+  KEY `ingredient` (`id_ingredient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contient`
+--
+
+DROP TABLE IF EXISTS `contient`;
+CREATE TABLE IF NOT EXISTS `contient` (
+  `id_boisson` int DEFAULT NULL,
+  `id_menu` int DEFAULT NULL,
+  KEY `boisson` (`id_boisson`),
+  KEY `menu` (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `focaccia`
+--
+
+DROP TABLE IF EXISTS `focaccia`;
+CREATE TABLE IF NOT EXISTS `focaccia` (
+  `id_focaccia` int NOT NULL AUTO_INCREMENT,
+  `nom_focaccia` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prix_focaccia` float NOT NULL,
+  PRIMARY KEY (`id_focaccia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient`
+--
+
+DROP TABLE IF EXISTS `ingredient`;
+CREATE TABLE IF NOT EXISTS `ingredient` (
+  `id_ingredient` int NOT NULL AUTO_INCREMENT,
+  `nom_ingredient` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_ingredient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `marque`
+--
+
+DROP TABLE IF EXISTS `marque`;
+CREATE TABLE IF NOT EXISTS `marque` (
+  `id_marque` int NOT NULL AUTO_INCREMENT,
+  `nom_marque` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_marque`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id_menu` int NOT NULL AUTO_INCREMENT,
+  `nom_menu` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prix_menu` float NOT NULL,
+  `id_focaccia` int DEFAULT NULL,
+  PRIMARY KEY (`id_menu`),
+  KEY `FK` (`id_focaccia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paye`
+--
+
+DROP TABLE IF EXISTS `paye`;
+CREATE TABLE IF NOT EXISTS `paye` (
+  `jour` date NOT NULL,
+  `id_client` int DEFAULT NULL,
+  `id_menu` int DEFAULT NULL,
+  KEY `client` (`id_client`),
+  KEY `menu` (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `achete`
+--
+ALTER TABLE `achete`
+  ADD CONSTRAINT `achete` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `boisson`
+--
+ALTER TABLE `boisson`
+  ADD CONSTRAINT `marque` FOREIGN KEY (`id_marque`) REFERENCES `marque` (`id_marque`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `comprend`
+--
+ALTER TABLE `comprend`
+  ADD CONSTRAINT `comprend` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ingredient` FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id_ingredient`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `contient`
+--
+ALTER TABLE `contient`
+  ADD CONSTRAINT `boisson` FOREIGN KEY (`id_boisson`) REFERENCES `boisson` (`id_boisson`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `contient` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `focaccia` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `paye`
+--
+ALTER TABLE `paye`
+  ADD CONSTRAINT `menu` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `paye` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
